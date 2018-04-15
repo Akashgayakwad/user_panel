ac579105bf1185d655e1ffad4100bf902c529fbe
Akash Gayakwad <akashgayakwad123@gmail.com>
2018-04-15 16:08:51 +0530

    typo fix

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=screen-width intial-scale=1.0 user-scalable=yes">
+    <meta name="viewport" content="width=device-width initial-scale=1.0 user-scalable=yes">
     <title>Login/Signup</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
       <link href="style.css" rel="stylesheet">
